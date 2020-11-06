7471db0d2b4c0e16b987910531988fb60e58e644
Crashk1 <crashkofslug@gmail.com>
2020-11-05 23:33:38 -0300

    primer commit

---
+++ src/practica1/Jugador.java
@@ -0,0 +1,33 @@
+package practica1;
+
+public class Jugador {
+	
+	private Integer valor1;
+	private Integer valor2;
+	private Integer valor3;
+	private Integer valor4;
+	
+	public Jugador(Integer n1, Integer n2, Integer n3, Integer n4) {
+		this.valor1 = n1;
+		this.valor2 = n2;
+		this.valor3 = n3;
+		this.valor4 = n4;
+	}
+	
+	public Integer getValor1() {
+		return valor1;
+	}
+	
+	public Integer getValor2() {
+		return valor2;
+	}
+	
+	public Integer getValor3() {
+		return valor3;
+	}
+	
+	public Integer getValor4() {
+		return valor4;
+	}
+	
+}
---
+++ test/practica1/JugadorTest.java
@@ -0,0 +1,26 @@
+package practica1;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class JugadorTest {
+
+	@Test
+	public void queValor1RetorneCorrectamente() {
+		Jugador maradona = new Jugador(1,2,3,4);
+		Integer ve=1;
+		
+		assertEquals(ve,maradona.getValor1());
+	}
+
+	
+	@Test
+	public void queValor2RetorneCorrectamente() {
+		Jugador maradona = new Jugador(1,2,3,4);
+		Integer ve=2;
+		
+		assertEquals(ve,maradona.getValor2());
+	}
+	
+}
